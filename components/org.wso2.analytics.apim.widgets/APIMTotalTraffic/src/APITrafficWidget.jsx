fd638ebed1ee7a1b70010684066e0ddbc2c0f02d
Thidasala <thidasala.demintha@gmail.com>
2019-10-21 17:28:37 +0530

    Add the APIM Traffic Widget

---
+++ components/org.wso2.analytics.apim.widgets/APIMTotalTraffic/src/APITrafficWidget.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+ 
+class APITrafficWidget extends Component {
+    render() {
+        return (
+            <h1>Hello, World!sws</h1>
+        );
+    }
+}
+ 
+global.dashboard.registerWidget('APITrafficWidget', APITrafficWidget);
